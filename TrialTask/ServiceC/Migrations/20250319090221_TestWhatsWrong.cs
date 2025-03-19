6a3a273e813fbe25d4c885bb2012bb3bd60a675a
Emin <emin.sarux2004@yandex.ru>
2025-03-19 12:06:36 +0300

    forgot to commit migration

---
+++ TrialTask/ServiceC/Migrations/20250319090221_TestWhatsWrong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceC.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestWhatsWrong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Time",
+                table: "WeatherInfo",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Time",
+                table: "WeatherInfo",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
